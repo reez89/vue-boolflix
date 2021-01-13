ceae644f9d68d6ea5c2866f08387f8f7875fca62
Nicola R <nicola.rizzi89@hotmail.it>
2021-01-13 10:59:31 +0100

    Milestone 4
    Css base

--- milestone3/main.js
+++ milestone3/main.js
@@ -1,13 +1,11 @@
-/* In questa milestone come prima cosa aggiungiamo la copertina del film o della serie
-al nostro elenco. Ci viene passata dall’API solo la parte finale dell’URL, questo
-perché poi potremo generare da quella porzione di URL tante dimensioni diverse.
-Dovremo prendere quindi l’URL base delle immagini di TMDB:
-https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare
-(troviamo tutte le dimensioni possibili a questo link:
-https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400) per poi aggiungere la
-parte finale dell’URL passata dall’API.
-Esempio di URL:
-https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
+/* Trasformiamo quello che abbiamo fatto fino ad ora in una vera e propria webapp,
+creando un layout completo simil-Netflix:
+● Un header che contiene logo e search bar
+● Dopo aver ricercato qualcosa nella searchbar, i risultati appaiono sotto forma
+di “card” in cui lo sfondo è rappresentato dall’immagine di copertina (consiglio
+la poster_path con w342)
+● Andando con il mouse sopra una card (on hover), appaiono le informazioni
+aggiuntive già prese nei punti precedenti più la overview
  */
 
 let app = new Vue({
@@ -25,9 +23,11 @@ let app = new Vue({
         searchMovie: function (search){
             
             axios.get(`https://api.themoviedb.org/3/search/movie?api_key=63c44cc4459f95138303a72049a37548&language=it&query=${search}&include_adult=false`).then(resp=>{
+
                let film = resp.data.results
                
             axios.get(`https://api.themoviedb.org/3/search/tv?api_key=63c44cc4459f95138303a72049a37548&language=it&query=${search}&include_adult=false`).then(resp=>{
+                
                 let series = resp.data.results
 
                 //Aggiungo entrambe le richieste al mio database generale.
