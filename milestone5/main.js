a70eef2805671c85dabade50f195804d5b830e6b
Nicola R <nicola.rizzi89@hotmail.it>
2021-01-13 16:42:11 +0100

    Milestone 5 da cominciare.
    On going.

--- milestone5/main.js
+++ milestone5/main.js
@@ -1,12 +1,8 @@
-/* Milestone 4:
-Trasformiamo quello che abbiamo fatto fino ad ora in una vera e propria webapp,
-creando un layout completo simil-Netflix:
-● Un header che contiene logo e search bar
-● Dopo aver ricercato qualcosa nella searchbar, i risultati appaiono sotto forma
-di “card” in cui lo sfondo è rappresentato dall’immagine di copertina (consiglio
-la poster_path con w342)
-● Andando con il mouse sopra una card (on hover), appaiono le informazioni
-aggiuntive già prese nei punti precedenti più la overview */
+/* Partendo da un film o da una serie, richiedere all'API quali sono gli attori che fanno
+parte del cast aggiungendo alla nostra scheda Film / Serie SOLO i primi 5 restituiti
+dall’API con Nome e Cognome, e i generi associati al film con questo schema:
+“Genere 1, Genere 2, …”.
+ */
 
 let app = new Vue({
     el: "#app",
@@ -39,6 +35,7 @@ let app = new Vue({
                 });
 
                 this.filmsDb.forEach(element=>{
+
                     if (element.original_language == "en"){
                         element.original_language = "gb";
                     } else if (element.original_language == "zh") {
