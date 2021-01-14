628373b2be53d65a19db246070aefdab15c103f3
Nicola R <nicola.rizzi89@hotmail.it>
2021-01-14 16:50:03 +0100

    Milestone 5
    Errore undifined risolto, genres on going.

--- milestone5/main.js
+++ milestone5/main.js
@@ -11,8 +11,12 @@ let app = new Vue({
         filmsDb:[],
     },
 
+    refresh(){
+        this.$forceUpdate()
+    },
+
     mounted() {
-        this.getCast();
+        
     },
     
     methods: {
@@ -68,7 +72,6 @@ let app = new Vue({
 
                 // per poter aggiungere ad ogni elemento, la proprietà cast, uso un forEach.
                 this.filmsDb.forEach(movie=> this.sumCast(movie))
-                
             })
 
         })
@@ -77,7 +80,7 @@ let app = new Vue({
 
        // Creo una funzione che mi permette di reperire l'id del mio video. Visto che a volte gli id hanno meno di 5 cifre, devo utilizzare 2 cicli differenti per poter prendere ogni risultato.
 
-        getCast: function(movie_id){
+        getMovieCast: function(movie_id){
             let movieCast = [];
             axios
                 .get(`https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=63c44cc4459f95138303a72049a37548`)
@@ -85,7 +88,7 @@ let app = new Vue({
                     let cast = resp.data.cast;
                     if(cast.length>=5){
                         for (let i=0; i<5; i++) {
-                            console.log(cast[i].name);
+                            // console.log(cast[i].name);
                             movieCast.push(cast[i].name)
                         } 
                     }else if (cast.length < 5) {
@@ -100,17 +103,47 @@ let app = new Vue({
                 return movieCast;
         },
 
+        getTvCast: function(tv_id){
+            let tvCast = [];
+            axios
+                .get(`https://api.themoviedb.org/3/tv/${tv_id}/credits?api_key=63c44cc4459f95138303a72049a37548`)
+                .then(resp=>{
+                    let cast = resp.data.cast;
+                    if(cast.length>=5){
+                        for (let i=0; i<5; i++) {
+                            console.log(cast[i].name);
+                            tvCast.push(cast[i].name)
+                        } 
+                    }else if (cast.length < 5) {
+                        for (let i = 0; i < cast.length; i++) {
+                            tvCast.push(cast[i].name);
+                        }
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+                return tvCast;
+        },
+
+        getGenres: function(){
+            let genres = [];
+            axios
+                .get('https://api.themoviedb.org/3/genre/movie/list?api_key=63c44cc4459f95138303a72049a37548')
+                .then
+        },
+
         // Con questa funzione, inserisco, tramite il metodo Vue.set, una nuova proprità all'interno della mia istanza vue, che successivamente, nella fuzione search in alto, aggiungerò all'interno del mio filmsDB.
 
         sumCast(movie) {
-            if (movie.hasOwnProperty("original_title")) {
-                return Vue.set(movie, "cast", this.getCast(movie.id));
+            if (movie.hasOwnProperty("original_name")) {
+                Vue.set(movie, "cast", this.getTvCast(movie.id));
+            } else if (movie.hasOwnProperty("original_title")){
+                Vue.set(movie, "cast", this.getMovieCast(movie.id));
             }
         },
 
-        refresh(){
-            this.$forceUpdate()
-        }
+        
     }
 })
 
